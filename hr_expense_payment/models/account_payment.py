5f7aadb98e50dc6ea831e7a5fe970b7be9d24468
Don Kendall <dkendall@ledoweb.com>
2026-05-30 10:57:07 -0400

    [MIG] hr_expense_payment: Migration to 19.0
    hr.expense.sheet was removed in 19.0; retarget the payment register to hr.expense.

--- hr_expense_payment/models/account_payment.py
+++ hr_expense_payment/models/account_payment.py
@@ -1,5 +1,6 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
 # Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# Copyright 2026 Ledo <https://ledoweb.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -8,12 +9,12 @@ from odoo import fields, models
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
-    expense_sheet_ids = fields.Many2many(
-        comodel_name="hr.expense.sheet",
-        relation="payment_expense_sheet_rel",
+    expense_ids = fields.Many2many(
+        comodel_name="hr.expense",
+        relation="payment_hr_expense_rel",
         column1="payment_id",
-        column2="sheet_id",
-        string="Expense sheet",
+        column2="expense_id",
+        string="Expenses",
         readonly=True,
         copy=False,
     )
--- hr_expense_payment/tests/test_hr_expense_payment.py
+++ hr_expense_payment/tests/test_hr_expense_payment.py
@@ -1,6 +1,7 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
 # Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # Copyright 2024 Tecnativa - Víctor Martínez
+# Copyright 2026 Ledo <https://ledoweb.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests import Form, tagged
@@ -15,38 +16,41 @@ class TestHrExpensePayment(TestExpenseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Create expense + sheet + approve
-        cls.expense = cls.create_expense(cls)
-        res = cls.expense.action_submit_expenses()
-        cls.expense_sheet = cls.env[res["res_model"]].browse(res["res_id"])
-        cls.expense_sheet.action_submit_sheet()
-        cls.expense_sheet.action_approve_expense_sheets()
+        # Build an employee-paid expense, submit + approve + post (= receipt).
+        cls.expense = cls.create_expenses({"payment_mode": "own_account"})
+        cls.expense.action_submit()
+        cls.expense.action_approve()
+        cls.post_expenses_with_wizard(cls.expense)
 
     def _get_payment_wizard(self):
-        res = self.expense_sheet.action_register_payment()
-        register_form = Form(self.env[res["res_model"]].with_context(**res["context"]))
-        register_form.journal_id = self.company_data["default_journal_bank"]
-        register_form.amount = self.expense_sheet.total_amount
-        return register_form.save()
+        action = self.expense.action_pay()
+        wizard_form = Form(
+            self.env[action["res_model"]].with_context(**action["context"])
+        )
+        wizard_form.journal_id = self.company_data["default_journal_bank"]
+        wizard_form.amount = self.expense.total_amount
+        return wizard_form.save()
 
-    def test_post_init_hook(self):
-        self.expense_sheet.action_sheet_move_post()
-        payment_wizard = self._get_payment_wizard()
-        payment_wizard.action_create_payments()
-        payment = self.expense_sheet.payment_ids
+    def test_action_pay_links_payment_back_to_expense(self):
+        """After action_pay → create_payment, the resulting payment's
+        expense_ids includes the source expense and the expense's
+        payment_ids includes the new payment."""
+        self.assertFalse(self.expense.payment_ids)
+        wizard = self._get_payment_wizard()
+        wizard.action_create_payments()
+        self.assertEqual(len(self.expense.payment_ids), 1)
+        payment = self.expense.payment_ids
+        self.assertIn(self.expense, payment.expense_ids)
+
+    def test_post_init_hook_backfills_legacy_payments(self):
+        """post_init_hook walks reconciliation to recover the back-link for
+        payments that pre-date the module install."""
+        wizard = self._get_payment_wizard()
+        wizard.action_create_payments()
+        payment = self.expense.payment_ids
         self.assertEqual(len(payment), 1)
-        self.assertEqual(len(payment.expense_sheet_ids), 1)
-        payment.expense_sheet_ids = False
-        # Recompute many2one
-        payment = self.expense_sheet.payment_ids
-        self.assertFalse(payment)
-        self.assertFalse(payment.expense_sheet_ids)
+        # Wipe the back-link, then re-run the hook.
+        payment.expense_ids = False
+        self.assertFalse(self.expense.payment_ids)
         post_init_hook(self.env)
-        self.assertEqual(len(self.expense_sheet.payment_ids), 1)
-
-    def test_get_payment_vals(self):
-        self.expense_sheet.action_sheet_move_post()
-        payment_wizard = self._get_payment_wizard()
-        self.assertFalse(self.expense_sheet.payment_ids)
-        payment_wizard.action_create_payments()
-        self.assertEqual(len(self.expense_sheet.payment_ids), 1)
+        self.assertEqual(len(self.expense.payment_ids), 1)
